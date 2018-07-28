b11709c20e2cc4891a78bed1249cdd0431c495f8
Spomi <40448323+Spomi@users.noreply.github.com>
2018-07-28 13:09:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1092,6 +1092,16 @@ bot.on('message', msg => {
 })
 
 
+client.on('message', message => {
+       if (message.content.startsWith(prefix + 'botinfo')) {
+     let msg =  client.guilds.map(guild => `**${guild.name}** عدد الاعضاء: ${guild.memberCount}`).join('\n');
+  let embed = new Discord.RichEmbed()
+  .setTitle(`${client.guilds.size}سيرفرات `)
+  .setDescription(`${msg}`)
+  .setColor("#ebf442");
+  message.channel.send(embed);
+}
+});
 
 
 
