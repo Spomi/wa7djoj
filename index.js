a05541b10dee6fffe63d94846186e451c20176db
Spomi <40448323+Spomi@users.noreply.github.com>
2018-08-16 09:27:50 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1335,7 +1335,89 @@ eyad.channel.sendEmbed(Embed11).then(eyad => {eyad.delete(20000)})
   
 
  
-  
+
+client.on('message', message => {
+   if(!message.channel.guild) return;
+if(message.content.startsWith(prefix + 'upclear')) {
+if(!message.channel.guild) return message.channel.send('**This Command is Just For Servers**').then(m => m.delete(5000));
+if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**You Do not have permission** `ADMINISTRATOR`' );
+let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
+let request = `Requested By ${message.author.username}`;
+message.channel.send(`**Are You sure you want to clear the chat?**`).then(msg => {
+msg.react('✅')
+.then(() => msg.react('❌'))
+.then(() =>msg.react('✅'))
+
+let reaction1Filter = (reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id;
+let reaction2Filter = (reaction, user) => reaction.emoji.name === '❌' && user.id === message.author.id;
+
+let reaction1 = msg.createReactionCollector(reaction1Filter, { time: 12000 });
+let reaction2 = msg.createReactionCollector(reaction2Filter, { time: 12000 });
+reaction1.on("collect", r => {
+message.channel.send(`Chat will delete`).then(m => m.delete(5000));
+var msg;
+        msg = parseInt();
+
+      message.channel.fetchMessages({limit: msg}).then(messages => message.channel.bulkDelete(messages)).catch(console.error);
+      message.channel.sendMessage("", {embed: {
+        title: "`` Chat Deleted ``",
+        color: 0x06DF00,
+        footer: {
+
+        }
+      }}).then(msg => {msg.delete(3000)});
+
+})
+reaction2.on("collect", r => {
+message.channel.send(`**Chat deletion cancelled**`).then(m => m.delete(5000));
+msg.delete();
+})
+})
+}
+});
+ 
+ 
+
+
+client.on('guildMemberAdd', eyad => {
+  let channel = eyad.guild.channels.find('name', 'log');
+  let memberavatar = eyad.user.avatarURL
+    if (!channel) return;
+  let embed = new Discord.RichEmbed()
+      .setColor('GREEN')
+      .setThumbnail(memberavatar)
+      .setTitle('Member joined')
+  .setTitle('Member Joined')
+      .addField('UserName : ',`${eyad}`)
+      .addField('User Id :', "**[" + `${eyad.id}` + "]**" )
+      .addField('MemberCount :',`${eyad.guild.memberCount}`)
+      .addField('Server: ', `${eyad.guild.name}`,true)                          
+      .setFooter(" Join Log ! ")
+      .setTimestamp()
+ 
+    channel.sendEmbed(embed);
+  });
+
+
+client.on('guildMemberRemove', eyad => {
+  let channel = eyad.guild.channels.find('name', 'log');
+  let memberavatar = eyad.user.avatarURL
+    if (!channel) return;
+  let embed = new Discord.RichEmbed()
+      .setColor('RED')
+      .setThumbnail(memberavatar)
+  .setTitle('Member Left')
+      .addField('UserName : ',`${eyad}`)
+      .addField('User Id :', "**[" + `${eyad.id}` + "]**" )
+      .addField('MemberCount :',`${eyad.guild.memberCount}`)
+      .addField('Server: ', `${eyad.guild.name}`,true)                          
+      .setFooter(" Leave Log ! ")
+      .setTimestamp()
+ 
+    channel.sendEmbed(embed);
+      });
+
+
 
 
 
